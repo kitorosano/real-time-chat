cc7c9a360392a6f9a7018cd229a499ef237f6ad3
Kitorosano <rosanoesteban2@gmail.com>
2023-12-22 16:06:09 -0300

    first commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import logger from 'morgan';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
+import { db } from '@vercel/postgres';
 
 const port = process.env.PORT || 3000;
 
@@ -18,22 +21,55 @@ server.listen(port, () => {
 });
 
 const io = new Server(server, {
-  // the backup duration of the sessions and the packets
-  maxDisconnectionDuration: 2 * 60 * 1000,
-  // whether to skip middlewares upon successful recovery
-  skipMiddlewares: true
+  connectionStateRecovery: {}
 });
-io.on('connection', (socket) => {
-  console.log('a user has connected!');
-  if (socket.recovered) {
-    console.log('this socket was recovered from a client-side disconnect');
-  }
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+const client = await db.connect();
+try {
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS messages (
+      id SERIAL PRIMARY KEY,
+      content TEXT NOT NULL,
+      username TEXT NOT NULL
+    );
+  `;
+} catch (e) {
+  console.error(e);
+}
+
+io.on('connection', async (socket) => {
+  console.log('a user has connected!');
 
   socket.on('disconnect', () => {
     console.log('a user has disconnected!');
   });
+
+  socket.on('chat message', async (msg) => {
+    let result;
+    const username = socket.handshake.auth.username ?? 'anonymous';
+    try {
+      result = await client.sql`
+        INSERT INTO messages (content, username) VALUES (${msg}, ${username});
+      `;
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+
+    io.emit('chat message', msg, result.id, username);
+  });
+
+  if (!socket.recovered) {
+    try {
+      const serverOffset = socket.handshake.auth.serverOffset ?? 0;
+      const result = await client.sql`
+        SELECT id, content, username FROM messages WHERE id > ${serverOffset};
+      `;
+      for (const row of result.rows) {
+        socket.emit('chat message', row.content, row.id, row.username);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
 });
